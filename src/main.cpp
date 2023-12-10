e959ae36e3038c07a9aae8207803e4e7849200b6
estradax <azharumuhammad@gmail.com>
2023-12-10 14:34:15 +0700

    add fire animation

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ public:
   Vector(float x, float y) : x_{x}, y_{y} {}
 
   void SetX(float x) { x_ = x; }
+  void SetY(float y) { y_ = y; }
 
   float x() const { return x_; }
   float y() const { return y_; }
@@ -68,6 +69,10 @@ void DrawTriangle(Vector point, float b, float h) {
   glEnd();
 }
 
+Vector fire_position_1{34, 27};
+Vector fire_position_2{35, 29};
+Vector fire_position_3{35, 25};
+
 void DrawFirebon() {
   glColor3f(0.431, 0.278, 0.165);
   DrawLog({29, 30}, 1, 7, 20);
@@ -90,10 +95,24 @@ void DrawFirebon() {
   glEnd();
 
   glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(34, 27);
-    glVertex2f(35, 29);
-    glVertex2f(35, 25);
+    glVertex2f(fire_position_1.x(), fire_position_1.y());
+    glVertex2f(fire_position_2.x(), fire_position_2.y());
+    glVertex2f(fire_position_3.x(), fire_position_3.y());
   glEnd();
+
+  fire_position_1.SetY(fire_position_1.y() - 0.1);
+  fire_position_2.SetY(fire_position_2.y() - 0.1);
+  fire_position_3.SetY(fire_position_3.y() - 0.1);
+
+  if (fire_position_1.y() <= 20) {
+    fire_position_1.SetY(27);
+  }
+  if (fire_position_2.y() <= 22) {
+    fire_position_2.SetY(29);
+  }
+  if (fire_position_3.y() <= 18) {
+    fire_position_3.SetY(25);
+  }
 
   glColor3f(0.651, 0.647, 0.565);
   glBegin(GL_QUADS);
